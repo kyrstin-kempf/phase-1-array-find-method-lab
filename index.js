79707f760f4c8c4042512b30de0a8d2d8cd4f4eb
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-02-21 11:49:04 -0500

    commit tree d5cdc31cded0d742a63000e82be20fee0778d506

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// code your solution here
+function superbowlWin(record) {
+let winYear = record.find(record => record.result === "W");
+if (winYear) {
+        return winYear.year;
+    }
+}
+
+// function superbowlWin(array) {
+//     let winningYear = array.find((array) => array.result === "W").year;
+//     if (winningYear) {
+//       return winningYear;
+//     }
+//   }
+//   superbowlWin(record);
+
+//----------EXAMPLE ONE
+// const inventory = [
+//     {name: 'apples', quantity: 2},
+//     {name: 'bananas', quantity: 0},
+//     {name: 'cherries', quantity: 5}
+//   ];
+  
+//   const result = inventory.find( ({ name }) => name === 'cherries' );
+  
+//   console.log(result) // { name: 'cherries', quantity: 5 }
+
+//----------EXAMPLE TWO
+// const trees = [
+//     { name: "birch", count: 4 },
+//     { name: "maple", count: 5 },
+//     { name: "oak", count: 2 }
+//   ];
+  
+//   const result = trees.find(tree => tree.name === "oak");
+  
+//   // { name: "oak", count, 2 }
